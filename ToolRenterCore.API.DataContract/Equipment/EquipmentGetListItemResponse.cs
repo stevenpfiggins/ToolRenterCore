024bfcfddb03b10a671164d6796099c0f13c55b7
stevenpfiggins <sfiggins43@gmail.com>
2019-06-03 11:37:41 -0400

    Added EquipmentType and UserProfile models. Also updated Equipment with EquipmentType

--- ToolRenterCore.API.DataContract/Equipment/EquipmentGetListItemResponse.cs
+++ ToolRenterCore.API.DataContract/Equipment/EquipmentGetListItemResponse.cs
@@ -9,6 +9,7 @@ namespace ToolRenterCore.API.DataContract.Equipment
         public int EquipmentEntityId { get; set; }
         public int OwnerId { get; set; }
         public string EquipmentName { get; set; }
+        public int EquipmentTypeEntityId { get; set; }
         public string EquipmentDescription { get; set; }
         public decimal EquipmentRate { get; set; }
     }
---
+++ ToolRenterCore.API.DataContract/UserProfile/UserProfileGetListItemResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolRenterCore.API.DataContract.UserProfile
+{
+    public class UserProfileGetListItemResponse
+    {
+        public int UserProfileEntityId { get; set; }
+        public int OwnerId { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
